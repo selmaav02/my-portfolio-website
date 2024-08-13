b5b48be31b80865d3bd3cc0246a899dcf143ddf4
selmaav02 <selmaav02@gmail.com>
2024-08-13 17:29:44 +0530

    the invalidate color is changed

--- js/main.js
+++ js/main.js
@@ -105,13 +105,13 @@
 
 
     // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        dots: true,
-        loop: true,
-        items: 1
-    });
+    // $(".testimonial-carousel").owlCarousel({
+    //     autoplay: true,
+    //     smartSpeed: 1500,
+    //     dots: true,
+    //     loop: true,
+    //     items: 1
+    // });
     
 })(jQuery);
 
